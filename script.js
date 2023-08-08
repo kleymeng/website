cc403f177d82bdb424bb487dbe5463ae1bfd7093
pasha <140791929+kleymeng@users.noreply.github.com>
2023-08-08 20:44:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const downloadLink = document.getElementById('downloadLink');
 
 downloadLink.addEventListener('click', () => {
-    const cvUrl = 'URL_TO_YOUR_CV_FILE.pdf'; // Ganti dengan URL CV Anda
+    const cvUrl = 'pdf_20230801_083901_0000.pdf'; // Ganti dengan URL CV Anda
     downloadLink.href = cvUrl;
 });
